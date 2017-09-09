0dc2e1548f2831964fb7c853f23be825696b88bc
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-09 17:38:19 +0530

    Setting up the Router component for navigation purpose

--- manager/src/App.js
+++ manager/src/App.js
@@ -5,30 +5,28 @@ import ReduxThunk from 'redux-thunk';
 import firebase from 'firebase';
 import reducers from './reducers';
 
-import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 class App extends Component {
-
   componentWillMount() {
     // Initialize Firebase
     const config = {
-      apiKey: "AIzaSyAMoHLG6xpU1g4kC-IV0dN4_hsdexj1od8",
-      authDomain: "manager-13c73.firebaseapp.com",
-      databaseURL: "https://manager-13c73.firebaseio.com",
-      projectId: "manager-13c73",
-      storageBucket: "manager-13c73.appspot.com",
-      messagingSenderId: "1021374195327"
+      apiKey: 'AIzaSyAMoHLG6xpU1g4kC-IV0dN4_hsdexj1od8',
+      authDomain: 'manager-13c73.firebaseapp.com',
+      databaseURL: 'https://manager-13c73.firebaseio.com',
+      projectId: 'manager-13c73',
+      storageBucket: 'manager-13c73.appspot.com',
+      messagingSenderId: '1021374195327'
     };
     firebase.initializeApp(config);
   }
-
 
   render() {
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
     return (
       <Provider store={store}>
-        <LoginForm />
+        <Router />
       </Provider>
     );
   }
