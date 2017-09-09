2d41d7d81fcfc0dce52e9555c552d38f37418523
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-09 21:53:37 +0530

    Handled the form updates in the reducer employeeformreducer by using key interpolation to call the action.payload

--- manager/src/reducers/EmployeeFormReducer.js
+++ manager/src/reducers/EmployeeFormReducer.js
@@ -1,9 +1,14 @@
 import { EMPLOYEE_UPDATE } from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = { name: '', phone: '', shift: '' };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMPLOYEE_UPDATE:
+      //action.payload === { prop : 'name', value:'Srivatsa'}
+      //we are doing key interpolation this is not a array
+      return { ...state, [action.payload.prop]: action.payload.value };
+
     default:
       return state;
   }
