8f834114f1c377fb104812a03ff5e80af18d334c
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-11 00:23:07 +0530

    Modal component improved version with props but with no styling

--- manager/src/components/common/Confirm.js
+++ manager/src/components/common/Confirm.js
@@ -3,6 +3,28 @@ import { Text, View, Modal } from 'react-native';
 import { CardSection } from './CardSection';
 import { Button } from './Button';
 
-const Confirm = () => {};
+const Confirm = ({ children, visible, onAccept, onDecline }) => {
+  return (
+    <Modal
+      visible={visible}
+      animationType="slide"
+      transparent
+      onRequestClose={() => {}}
+    >
+      <View>
+        <CardSection>
+          <Text>
+            {children}
+          </Text>
+        </CardSection>
+
+        <CardSection>
+          <Button onPress={onAccept}>Yes</Button>
+          <Button onPress={onDecline}>No</Button>
+        </CardSection>
+      </View>
+    </Modal>
+  );
+};
 
 export { Confirm };
