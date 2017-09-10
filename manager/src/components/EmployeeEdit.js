edcb86e681788addb0b6b3db23eeb959673262a4
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-11 00:36:32 +0530

    Styling the modal and then setting a component level state for the display of the modal

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -3,10 +3,15 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 import { employeeUpdate, employeeSave } from '../actions';
 
 class EmployeeEdit extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showModal: false };
+  }
+
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -41,6 +46,20 @@ class EmployeeEdit extends Component {
         <CardSection>
           <Button onPress={this.onTextPress.bind(this)}>Text Schedule</Button>
         </CardSection>
+
+        <CardSection>
+          <Button
+            onPress={() => {
+              this.setState({ showModal: !this.state.showModal });
+            }}
+          >
+            Fire Employee
+          </Button>
+        </CardSection>
+
+        <Confirm visible={this.state.showModal}>
+          Are you sure you want to delete this ?
+        </Confirm>
       </Card>
     );
   }
